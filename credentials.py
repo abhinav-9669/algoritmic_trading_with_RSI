0cf94aff57a4b853f92b60e464acab92f1e5baca
Abhinav Kumar Sharma <145268578+abhinav-9669@users.noreply.github.com>
2023-09-19 09:30:34 +0530

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,11 @@
+#replace  username,pwd, api_key  with yours
+USER_NAME = '83yhgyw'
+PWD = 'ffhiwnc'
+API_KEY = 'wyywcuuy'
+
+#Dont change  below varibale 
+FEED_TOKEN = None
+TOKEN_MAP = None
+SMART_API_OBJ = None
+
+
